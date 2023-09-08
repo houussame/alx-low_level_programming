56671a5d6a8d93530e0bdbb3f127915a7bd0223c
houussame <2015.houdou@gmail.com>
2023-09-08 12:30:50 -0700

    prr

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	char ch = 'a';
-	char ch = 'A';
+	char CH = 'A';
 
 	/*prints a - z*/
 	while (ch <= 'z')
@@ -25,7 +25,7 @@ int main(void)
 		putchar(CH);
 		CH++;
 	}
-	putchat('\n');
+	putchar('\n');
 
 	return (0);
 }
