a24c6bb9f795cc96e420b821bafb6cf123a5bfea
houussame <2015.houdou@gmail.com>
2023-09-09 12:20:16 -0700

    pp

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: using sizeof to print the size of various type.
+ * Description: using sizeof to print the size of various type
  *
  * Return: Always 0 (success)
 */
