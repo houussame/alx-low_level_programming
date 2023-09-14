5f9756cdc7d20f001bbf662126ec37510cbe8d94
houussame <2015.houdou@gmail.com>
2023-09-14 14:23:52 -0700

    print

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,14 +4,14 @@
  *print_line - print a straight line
  *
  * @n: is the number of times the _ character
- *	should be printed 
+ *	should be printed
 */
 
 void print_line(int n)
 {
 	int lnChr;
 
-	if (n <=0)
+	if (n <= 0)
 		_putchar('\n');
 	else
 	{
