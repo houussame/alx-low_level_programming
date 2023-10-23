03d403625e699b7d9ced65a88344ac49cd3a8ee8
houussame <2015.houdou@gmail.com>
2023-10-23 11:58:38 -0700

    print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,12 +3,14 @@
 /**
  *print_listint - function that prints all the elements
  *@h:pointer to first node
- *
  *Return: number of nodes
 */
 size_t print_listint(const listint_t *h)
 {
 	size_t a = 0;
+
+	if (!h)
+		return (0);
 
 	while (h)
 	{
