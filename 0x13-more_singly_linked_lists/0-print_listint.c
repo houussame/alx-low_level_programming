f7974bcfc02ab663145833f441c973b589ee5638
houussame <2015.houdou@gmail.com>
2023-10-23 11:42:17 -0700

    print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,13 +7,13 @@
 */
 size_t print_listint(const listint_t *h)
 {
-	size_t a = 0;
+	size_t i = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		a++;
+		i++;
 	}
-	return (a);
+	return (i);
 }
