66ecbb2ed214447138db789326520e1a747624ae
houussame <2015.houdou@gmail.com>
2023-10-24 12:00:45 -0700

    get

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 unsigned int a = 0;
 listint_t *node = head;
 
-while (node && i < index)
+while (node && a < index)
 {
 node = node->next;
 a++;
